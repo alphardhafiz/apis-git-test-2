d581f8e1681b87ee8d6638f5307c9bd14607acd0
alphardhafiz <bucialphard@gmail.com>
2022-12-19 17:54:23 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum aliquid accusantium reiciendis earum similique ex eveniet inventore facilis aspernatur harum laudantium neque nemo et ut quidem, repellendus fuga laborum ea.</p>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+  </ul>
 </body>
 </html>
